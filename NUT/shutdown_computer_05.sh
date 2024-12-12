90498608101eec8479d4eb273007ed92028aea78
boomam <37086258+boomam@users.noreply.github.com>
2024-12-12 13:26:36 -0500

    Create shutdown_computer_05.sh

---
+++ NUT/shutdown_computer_05.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the remote host and user
+REMOTE_USER="username"
+REMOTE_HOST="ip_address"
+
+# Define the command to run on the remote host
+REMOTE_COMMAND="/usr/sbin/shutdown"  # Replace with your desired command
+
+# Define the log file to store the output
+LOG_FILE="/data/logfile_computer_05.txt"
+
+# Execute the command on the remote host and capture the output
+ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND" >> "$LOG_FILE" 2>&1
+
+# Add a timestamp to the log
+echo "Executed on: $(date)" >> "$LOG_FILE"
+echo "---------------------------------------" >> "$LOG_FILE"
